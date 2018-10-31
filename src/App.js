ca52f01bf76ef6a6810373513a49c0a2c662fad3
Austin Hou <austhou@gmail.com>
2018-10-31 15:11:05 -0700

    a

--- src/App.js
+++ src/App.js
@@ -11,21 +11,21 @@ import ContentFrame from './components/ContentFrame';
 
 class App extends Component {
 
-  constructor() {
+constructor() {
     super();
-  }
+}
 
   componentWillMount() {
     const config = {
-      apiKey: "AIzaSyD760vImnqQ_CC_cV9XYt35iKfLt9QpGz0",
-      authDomain: "scribe-0.firebaseapp.com",
-      databaseURL: "https://scribe-0.firebaseio.com",
-      projectId: "scribe-0",
-      storageBucket: "scribe-0.appspot.com",
-      messagingSenderId: "464252124849"
+        apiKey: "AIzaSyD760vImnqQ_CC_cV9XYt35iKfLt9QpGz0",
+        authDomain: "scribe-0.firebaseapp.com",
+        databaseURL: "https://scribe-0.firebaseio.com",
+        projectId: "scribe-0",
+        storageBucket: "scribe-0.appspot.com",
+        messagingSenderId: "464252124849"
     };
     if (!firebase.apps.length) {
-      firebase.initializeApp(config);
+         firebase.initializeApp(config);
     }
     document.title = 'scribe viewer'
   }
@@ -34,7 +34,7 @@ class App extends Component {
     return (
       <div class="marginControl">
         <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-          <ContentFrame />
+            <ContentFrame />
         </Provider>
       </div>
     );
